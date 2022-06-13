f851ff924736cd0a414e83d3fe01acc169dc8fc8
Vinnicyus Gracindo <vinnicyus.gracindo@mercadolivre.com>
2022-06-13 17:29:04 -0300

    add gowitter entity

---
+++ internal/entities/gowitter.go
@@ -0,0 +1,8 @@
+package entities
+
+type Gowitter struct {
+	UUID      string `json:"uuid"`
+	UserID    string `json:"user_id"`
+	Text      string `json:"text"`
+	CreatedAt string `json:"created_at"`
+}
